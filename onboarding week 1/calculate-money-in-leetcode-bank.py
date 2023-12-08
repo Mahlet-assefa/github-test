a682ac40f4011767dfc15528f6342ce3c73d7964
Mahlet Assefa <mahletassefa07@gmail.com>
2023-12-08 16:26:47 +0300

    Added solution for Calculate Money in Leetcode Bank

---
+++ onboarding week 1/calculate-money-in-leetcode-bank.py
@@ -0,0 +1,19 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+      def weekly(start,end):
+            return sum(range(start,end + 1))
+        
+      full_week = n // 7
+      half_week = n % 7
+
+      total_sum = 0
+      for i in range(full_week):
+            total_sum += weekly(i + 1 , i + 7)
+
+      total_sum += weekly(1 + full_week,half_week + full_week)
+    
+      return total_sum
+
+      
+      
+      
