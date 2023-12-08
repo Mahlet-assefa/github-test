3971c53c8c4ca4bf29eed806250e222734c97bc7
Mahlet Assefa <mahletassefa07@gmail.com>
2023-12-08 10:21:25 +0300

    Added solution for Check if All the Integers in a Range Are Covered

---
+++ onboarding week 1/check-if-all-the-integers-in-a-range-are-covered.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        included = set()
+        for start, end in ranges:
+          for i in range(start, end + 1):
+            included.add(i)
+        for i in range(left,right+1):
+          if not i in included:
+            return False
+        return True
+                
+               
