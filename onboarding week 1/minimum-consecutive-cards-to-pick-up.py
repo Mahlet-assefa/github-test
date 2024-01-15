b80244fcdedcf9a5e57b08e0b3f8d7cd1355445a
Mahlet Assefa <mahletassefa07@gmail.com>
2024-01-15 23:31:28 +0300

    Added solution for Minimum Consecutive Cards to Pick Up

--- onboarding week 1/minimum-consecutive-cards-to-pick-up.py
+++ onboarding week 1/minimum-consecutive-cards-to-pick-up.py
@@ -1,28 +1,13 @@
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
-        my_dict=defaultdict(int)
-        res=0 
-        ans=float("inf")
-        for r in range(len(cards)):
-            if cards[r] not in my_dict:
-                my_dict[cards[r]]+=r
-                #print(my_dict)
-            else: 
-                res = r - my_dict[cards[r]]+1
-                my_dict[cards[r]]=r
-                ans=min(res,ans)
-        if res==0:
-            return -1
-        else:   
-            return ans 
+        seen = {}
+        result = float("inf")
+        for i, x in enumerate(cards):
+            if x in seen:
+                result = min(result, i-seen[x]+1)
+            seen[x] = i
+        if result != float("inf") :
+            return result 
+        else:
+             return -1
         
-        
-
-
-
-                
-        #     else:
-        #         my_set.add(cards[idx])
-                
-        # return -1
-                
